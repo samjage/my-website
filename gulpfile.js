c804a86bf6088cf8bf9e09086a5ef1cd2aa17eb7
Sam Jage <samjage@gmail.com>
2016-03-07 23:26:20 -0700

    my-website updated

--- gulpfile.js
+++ gulpfile.js
@@ -3,30 +3,70 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var autoprefixer = require('gulp-autoprefixer');
+var browserSync = require('browser-sync').create();
 var sassdoc = require('sassdoc');
 
 /* ====== Variables ====== */
 
 var input = './_src/sass/**/*.scss';
 var output = './_dist/css';
+var outputRoot = './_dist'
 var autoprefixerOptions = {
   browsers: ['last 2 versions', '> 5%', 'Firefox ESR']
 };
+
 var sassOptions = {
   errLogToConsole: true,
   outputStyle: 'compressed'
 };
 
+var filesToMove = [
+    './_src/javascript/*.*',
+    './_src/fonts/*.*',
+    './_src/*.html'
+];
+
 /* ====== Tasks ====== */
 
-/* SASS */
+// Static Server + watching scss/html files
+gulp.task('serve', ['sass', 'move', 'move:watch'], function() {
 
+    browserSync.init({
+        server: "./_dist"
+    });
+
+    gulp.watch(input, ['sass']);
+    gulp.watch('./_dist/*.html').on('change', browserSync.reload);
+});
+
+// Compile sass into CSS & auto-inject into browsers
 gulp.task('sass', function () {
+    return gulp
+        .src(input)
+        .pipe(sass(sassOptions).on('error', sass.logError))
+        .pipe(autoprefixer(autoprefixerOptions))
+        .pipe(gulp.dest(output))
+        .pipe(browserSync.stream());
+});
+
+/* Files To Move */
+
+gulp.task('move', function(){
+    // the base option sets the relative root for the set of files,
+    // preserving the folder structure
+    return gulp
+        .src(filesToMove, { base: './_src' })
+        .pipe(gulp.dest(outputRoot));
+});
+
+/* Files To Move (Watch) */
+
+gulp.task('move:watch', function() {
   return gulp
-    .src(input)
-    .pipe(sass(sassOptions).on('error', sass.logError))
-    .pipe(autoprefixer(autoprefixerOptions))
-    .pipe(gulp.dest(output))
+    .watch(filesToMove, ['move'])
+    .on('change', function(event) {
+      console.log('File ' + event.path + ' was ' + event.type + ', liking turtles...');
+    });
 });
 
 /* sassDoc */
@@ -38,18 +78,4 @@ gulp.task('sassdoc', function () {
     .resume();
 });
 
-/* ====== Tasks (Watched) ====== */
-
-/* SASS Updates */
-
-gulp.task('sass:watch', function() {
-  return gulp
-    .watch(input, ['sass'])
-    .on('change', function(event) {
-      console.log('File ' + event.path + ' was ' + event.type + ', running tasks...');
-    });
-});
-
-/* ====== Tasks (Default) ====== */
-
-gulp.task('default', ['sass', 'sass:watch']);
+gulp.task('default', ['serve']);
